00c66086683257a28bf179cff2bd1a1b5321cc3d
ChitreshN <chitreshnarra@gmail.com>
2023-04-17 00:52:20 +0530

    basic setup

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ for (var i = 0; i < 16 * 16; i++) {
     var box = document.createElement("div");
     box.className = "box";
     container.appendChild(box);
+    box.addEventListener("mouseover", function() {
+        this.style.backgroundColor = "black";
+    });
 }
+
+// have to add event listeners
+
