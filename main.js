5931704c680938e6f7133a2a69f8f16d9003dea8
ChitreshN <chitreshnarra@gmail.com>
2023-04-17 04:55:43 +0530

    add random color functionality

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+var mode = "rainbow";
+var cl = "black";
+
+function color_changer(e){
+    if(mode == 'rainbow'){
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        console.log("here");
+    }
+    else e.target.style.backgroundColor = cl;
+}
+
+
 var container = document.getElementById("container");
 for (var i = 0; i < 16 * 16; i++) {
     var box = document.createElement("div");
     box.className = "box";
     container.appendChild(box);
-    box.addEventListener("mouseover", function() {
-        this.style.backgroundColor = "black";
-    });
+    box.addEventListener("mouseover", color_changer);
 }
-
-// have to add event listeners
-
